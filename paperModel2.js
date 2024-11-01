12032bd09c336a86a8754b755c775bb843cb78ff
victormule <50982984+victormule@users.noreply.github.com>
2024-11-01 14:56:18 +0100

    Update paperModel2.js

--- paperModel2.js
+++ paperModel2.js
@@ -71,7 +71,6 @@ const sketch2 = (p) => {
             baseSize = p.max(80, 90 * scaleFactor);
             spacing = p.max(18, 24 * scaleFactor);
         } else {
-            rows = 2
             baseSize = p.max(40, 50 * scaleFactor); // Exemple: plus grand en paysage
             spacing = p.max(14, 20 * scaleFactor);  // Exemple: plus grand en paysage
         }
