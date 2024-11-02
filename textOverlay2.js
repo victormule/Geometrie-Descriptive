f7fcc7d94a50cda945267020fe45974677d40a55
victormule <50982984+victormule@users.noreply.github.com>
2024-11-02 18:43:44 +0100

    Update textOverlay2.js

--- textOverlay2.js
+++ textOverlay2.js
@@ -66,7 +66,7 @@ let textOverlay2 = new p5(function(sketch) {
 
         // Positions des textes
         sketch.titleY = sketch.height / 10 - 20;
-        sketch.subtitleY = sketch.height / 10 + 60;
+        sketch.subtitleY = sketch.height / 10 + 100;
         sketch.descriptionY = sketch.height / 1.6;
     }
 
