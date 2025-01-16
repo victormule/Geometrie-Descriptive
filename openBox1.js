c5a3ea080a675a8eca29c7dd45dc725bb4f1f325
victormule <50982984+victormule@users.noreply.github.com>
2025-01-16 08:21:33 +0100

    Update openBox1.js

--- openBox1.js
+++ openBox1.js
@@ -51,7 +51,7 @@ function setup() {
 }
 
 function draw() {
-  background(130, 100, 0, 80); // Fond coloré avec transparence
+  background(160, 130, 0, 80); // Fond coloré avec transparence
 
   // Calculer la distance de la souris par rapport au centre de l'écran
   let centerX = width / 2;
